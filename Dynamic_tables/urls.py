d87706f1b513059d4de0eb7a03298cba36fabc3d
Rahul Choudhary <rahul0101rock@gmail.com>
2022-01-19 23:58:38 +0530

    Setting up templates and basic routing

--- Dynamic_tables/urls.py
+++ Dynamic_tables/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Dtables.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
